4d8f0b75ccfb586b013da2603d131bf1ed80aa0f
Christian <ferchris82@hotmail.com>
2024-02-10 13:48:48 -0500

    Versión 0.1

---
+++ src/main/java/org/chrisferdev/hibernateapp/HibernateListar.java
@@ -0,0 +1,17 @@
+package org.chrisferdev.hibernateapp;
+
+import jakarta.persistence.EntityManager;
+import org.chrisferdev.hibernateapp.entity.Cliente;
+import org.chrisferdev.hibernateapp.util.JpaUtil;
+
+import java.util.List;
+
+public class HibernateListar {
+    public static void main(String[] args) {
+
+        EntityManager em = JpaUtil.getEntityManager();
+        List<Cliente> clientes = em.createQuery("SELECT c FROM Cliente c").getResultList();
+        clientes.forEach(System.out::println);
+        em.close();
+    }
+}
