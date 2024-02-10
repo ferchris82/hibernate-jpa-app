6a0daaed8d53316e5a49f64746ee41ee507cb266
Christian <ferchris82@hotmail.com>
2024-02-10 14:07:27 -0500

    Versión 0.1

---
+++ src/main/java/org/chrisferdev/hibernateapp/HibernateListarWhere.java
@@ -0,0 +1,24 @@
+package org.chrisferdev.hibernateapp;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+import org.chrisferdev.hibernateapp.entity.Cliente;
+import org.chrisferdev.hibernateapp.util.JpaUtil;
+
+import java.util.Scanner;
+
+public class HibernateListarWhere {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        EntityManager em = JpaUtil.getEntityManager();
+        Query query = em.createQuery("SELECT c FROM Cliente c WHERE c.formaPago=?1", Cliente.class);
+        System.out.println("Ingrese una forma de pago: ");
+        String pago = s.next();
+        query.setParameter(1, pago);
+        Cliente c = (Cliente) query.getSingleResult();
+        System.out.println(c);
+        em.close();
+    }
+}
---
+++ src/main/java/org/chrisferdev/hibernateapp/HibernatePorId.java
@@ -0,0 +1,32 @@
+package org.chrisferdev.hibernateapp;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+import org.chrisferdev.hibernateapp.entity.Cliente;
+import org.chrisferdev.hibernateapp.util.JpaUtil;
+
+import java.util.Scanner;
+
+public class HibernatePorId {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        /*EntityManager em = JpaUtil.getEntityManager();
+        Query query = em.createQuery("SELECT c FROM Cliente c WHERE c.id=?1", Cliente.class);
+        System.out.println("Ingrese el id: ");
+        Long pago = s.nextLong();
+        query.setParameter(1, pago);
+        Cliente c = (Cliente) query.getSingleResult();
+        System.out.println(c);
+        em.close();*/
+
+        System.out.println("Ingrese el id: ");
+        Long id = s.nextLong();
+        EntityManager em = JpaUtil.getEntityManager();
+        Cliente cliente = em.find(Cliente.class, id);
+
+        System.out.println(cliente);
+        em.close();
+    }
+}
