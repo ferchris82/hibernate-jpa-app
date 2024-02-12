c9c4311fa6f0f8460a6e9e45ca0883193f7943fa
Christian <ferchris82@hotmail.com>
2024-02-12 12:42:48 -0500

    Versión 0.1

--- src/main/java/org/chrisferdev/hibernateapp/HibernateQL.java
+++ src/main/java/org/chrisferdev/hibernateapp/HibernateQL.java
@@ -1,6 +1,8 @@
 package org.chrisferdev.hibernateapp;
 
+import com.mysql.cj.xdevapi.Client;
 import jakarta.persistence.EntityManager;
+import org.chrisferdev.hibernateapp.dominio.ClienteDto;
 import org.chrisferdev.hibernateapp.entity.Cliente;
 import org.chrisferdev.hibernateapp.util.JpaUtil;
 
@@ -21,7 +23,7 @@ public class HibernateQL {
                 .getSingleResult();
         System.out.println(cliente);
 
-        System.out.println("consulta solo el nombre por id");
+        System.out.println("====== consulta solo el nombre por id ======");
         String nombreCliente = em.createQuery("SELECT c.nombre FROM Cliente c WHERE c.id=:id", String.class)
                 .setParameter("id", 2L)
                 .getSingleResult();
@@ -47,6 +49,26 @@ public class HibernateQL {
             System.out.println("id" + idCli + ",nombre=" + nombreCli + ",apellido=" + apellidoCli);
         });
 //        }
+
+        System.out.println("======== consulta por cliente y forma de pago ========");
+        registros = em.createQuery("SELECT c, c.formaPago FROM Cliente c", Object[].class)
+                        .getResultList();
+        registros.forEach(reg -> {
+            Cliente c = (Cliente) reg[0];
+            String formaPago = (String) reg[1];
+            System.out.println("formaPago=" + formaPago + "," + c);
+        });
+
+        System.out.println("===== consulta que puebla y devuelve objeto entity de una clase personalizada");
+        clientes = em.createQuery("SELECT NEW Cliente(c.nombre, c.apellido) FROM Cliente c", Cliente.class)
+                        .getResultList();
+        clientes.forEach(System.out::println);
+
+        System.out.println("===== consulta que puebla y devuelve objeto de una clase personalizada");
+        List<ClienteDto> clientesDto = em.createQuery("SELECT NEW org.chrisferdev.hibernateapp.dominio.ClienteDto(c.nombre, c.apellido) FROM Cliente c", ClienteDto.class)
+                .getResultList();
+        clientesDto.forEach(System.out::println);
+
         em.close();
     }
 }
