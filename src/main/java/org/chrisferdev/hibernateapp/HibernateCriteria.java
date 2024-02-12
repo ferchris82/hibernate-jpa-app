e8e7faf70f97e9ade7deb394345bf3a2bdac2615
Christian <ferchris82@hotmail.com>
2024-02-12 16:35:26 -0500

    Versión 0.1

--- src/main/java/org/chrisferdev/hibernateapp/HibernateCriteria.java
+++ src/main/java/org/chrisferdev/hibernateapp/HibernateCriteria.java
@@ -37,7 +37,7 @@ public class HibernateCriteria {
         query = criteria.createQuery(Cliente.class);
         from = query.from(Cliente.class);
         ParameterExpression<String> nombreParamLike = criteria.parameter(String.class, "nombreParam");
-        query.select(from).where(criteria.like(criteria.upper(from.get("nombre")),criteria.upper(nombreParamLike)));
+        query.select(from).where(criteria.like(criteria.upper(from.get("nombre")), criteria.upper(nombreParamLike)));
         clientes = em.createQuery(query).setParameter("nombreParam", "%glo%")
                 .getResultList();
         clientes.forEach(System.out::println);
@@ -81,6 +81,40 @@ public class HibernateCriteria {
         query.select(from).where(criteria.and(p3, criteria.or(porNombre, porFormaPago)));
         clientes = em.createQuery(query).getResultList();
         clientes.forEach(System.out::println);
+
+        System.out.println("======= consultas con ORDER BY ASC DESC ========");
+        query = criteria.createQuery(Cliente.class);
+        from = query.from(Cliente.class);
+
+        query.select(from).orderBy(criteria.desc(from.get("nombre")), criteria.asc(from.get("apellido")));
+        clientes = em.createQuery(query).getResultList();
+        clientes.forEach(System.out::println);
+
+        System.out.println("====== consulta por id ======");
+        query = criteria.createQuery(Cliente.class);
+        from = query.from(Cliente.class);
+        ParameterExpression<Long> idParam = criteria.parameter(Long.class, "id");
+
+        query.select(from).where(criteria.equal(from.get("id"), idParam));
+
+        Cliente cliente = em.createQuery(query)
+                .setParameter("id", 1L)
+                .getSingleResult();
+        System.out.println(cliente);
+
+        System.out.println("====== consulta solo el nombre de los clientes =======");
+        CriteriaQuery<String> queryString = criteria.createQuery(String.class);
+        from = queryString.from(Cliente.class);
+        queryString.select(from.get("nombre"));
+        List<String> nombres = em.createQuery(queryString).getResultList();
+        nombres.forEach(System.out::println);
+
+        System.out.println("====== consulta solo el nombre de los clientes unicos con DISTINCT =======");
+        queryString = criteria.createQuery(String.class);
+        from = queryString.from(Cliente.class);
+        queryString.select(criteria.upper(from.get("nombre"))).distinct(true);
+        nombres = em.createQuery(queryString).getResultList();
+        nombres.forEach(System.out::println);
         em.close();
     }
 }
